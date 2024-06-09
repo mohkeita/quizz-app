9d856508edf7cae76292cb8c8ee6776a8565c91c
Mohamed Keita <mohkeita13@gmail.com>
2024-06-09 03:09:26 +0200

    Initial commit

---
+++ src/main/java/io/mohkeita/quizz_app/QuizzAppApiApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.quizz_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mohkeita/quizz_app/QuizzAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.quizz_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
